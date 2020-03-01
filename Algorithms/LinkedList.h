00ed4ee19d195918757a4df9f7c0dd6c8395f124
Piotr <piotrek194@poczta.fm>
2020-03-01 23:46:44 +0100

    add linked list

--- Algorithms/LinkedList.h
+++ Algorithms/LinkedList.h
@@ -1,7 +1,81 @@
 #pragma once
 
-
+template <typename T>
 struct Node
 {
+	T value;
+	Node* next;
+};
 
+template <typename T>
+class LinkedList
+{
+public:
+	LinkedList()
+	{
+		head = nullptr;
+		tail = nullptr;
+	}
+
+	~LinkedList()
+	{
+		Node<T>* next;
+		while (head != nullptr)
+		{
+			next = head->next;
+			delete head;
+			head = next;
+		}
+	}
+
+	void add_node(const T& value)
+	{
+		Node<T>* node = new Node;
+		node->value = value;
+		node->next = nullptr;
+
+		if (head == nullptr)
+		{
+			head = node;
+			tail = node;
+		}
+		else
+		{
+			tail->next = node;
+			tail = tail->next;
+		}
+	}
+
+	void remove_node(const T& value)
+	{
+		Node<T>* node = head;
+		while (node != nullptr)
+		{
+			if (node->value == value)
+			{
+				head = node->next;
+				delete node;
+			}
+
+			node = node->next;
+		}
+	}
+
+	constexpr size_t size()
+	{
+		size_t count = 0;
+		Node<T>* node = head;
+		while (node != nullptr)
+		{
+			node = node->next;
+			count++;
+		}
+		return count;
+	}
+
+	
+
+private:
+	Node* head;
+	Node* tail;
 };
